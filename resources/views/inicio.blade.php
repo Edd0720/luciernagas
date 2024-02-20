b1fe9c78af81159c85f0ed5bc5ea09124b129948
Edd0720 <edd.cazj0420@gmail.com>
2024-02-20 09:09:18 -0600

    primer commit

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="css/app.css">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
+   <title>Document</title>
+</head>
+<body>
+   <header>
+      <nav class="menu">
+         <ul>
+            <li><img src="{{asset("img/firefly.png")}}" class="firefly-icon"></li>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Iniciar sesión</a></li>
+            <li><a href="">Registrarse</a></li>
+         </ul>
+      </nav>
+   </header>
+</body>
+</html>
